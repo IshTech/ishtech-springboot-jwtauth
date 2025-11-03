411bdedddb191e2eef501981f07bad0a3b1d3cde
Muneer Ahmed Syed <muneer@ishtech.fi>
2025-11-03 17:08:44 +0200

    fix name of main and test class

--- ishtech-springboot-jwtauth-web/src/main/java/fi/ishtech/springboot/jwtauth/IshtechSpringbootJwtauthApplication.java
+++ ishtech-springboot-jwtauth-web/src/main/java/fi/ishtech/springboot/jwtauth/IshtechSpringbootJwtauthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IshtechSpringBootJwtApplication {
+public class IshtechSpringbootJwtauthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IshtechSpringBootJwtApplication.class, args);
+		SpringApplication.run(IshtechSpringbootJwtauthApplication.class, args);
 	}
 
 }
--- ishtech-springboot-jwtauth-web/src/test/java/fi/ishtech/springboot/jwtauth/IshtechSpringbootJwtauthApplicationTests.java
+++ ishtech-springboot-jwtauth-web/src/test/java/fi/ishtech/springboot/jwtauth/IshtechSpringbootJwtauthApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IshtechSpringBootJwtApplicationTests {
+class IshtechSpringbootJwtauthApplicationTests {
 
 	@Test
 	void contextLoads() {
