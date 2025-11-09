11b9b24f5a9a74bdb128771eca641c306f160ab7
Muneer Ahmed Syed <muneer@ishtech.fi>
2025-11-09 17:55:06 +0200

    user profile params and spec

---
+++ ishtech-springboot-jwtauth-lib/src/main/java/fi/ishtech/springboot/jwtauth/payload/params/UserProfileFilterParams.java
@@ -0,0 +1,46 @@
+package fi.ishtech.springboot.jwtauth.payload.params;
+
+import java.io.Serial;
+
+import fi.ishtech.base.payload.filter.BaseStandardNoIdEntityFilterParams;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ *
+ * @author Muneer Ahmed Syed
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class UserProfileFilterParams extends BaseStandardNoIdEntityFilterParams {
+
+	@Serial
+	private static final long serialVersionUID = -6062451974582082883L;
+
+	@Positive
+	private Long id;
+
+	private String firstName;
+
+	private String middleName;
+
+	private String lastName;
+
+	private String title;
+
+	private String prefix;
+
+	private String suffix;
+
+	private String mobilePhone;
+
+	private String defaultLang;
+
+	private Long addressId;
+
+	private String email;
+
+}
