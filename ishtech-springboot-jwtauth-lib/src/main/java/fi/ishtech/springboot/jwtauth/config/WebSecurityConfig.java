8cd82c44c790a5ab0158da3956ead7e38711f5c2
Muneer Ahmed Syed <muneer@ishtech.fi>
2026-04-18 20:10:11 +0300

    /about without login

--- ishtech-springboot-jwtauth-lib/src/main/java/fi/ishtech/springboot/jwtauth/config/WebSecurityConfig.java
+++ ishtech-springboot-jwtauth-lib/src/main/java/fi/ishtech/springboot/jwtauth/config/WebSecurityConfig.java
@@ -174,7 +174,8 @@ public class WebSecurityConfig {
 				"/api/v1/auth/signin",
 				"/api/v1/auth/signup",
 				"/api/v1/auth/forgot-password",
-				"/api/v1/auth/reset-password"
+				"/api/v1/auth/reset-password",
+				"/api/v1/auth/about"
 		);
 		// @formatter:on
 	}
