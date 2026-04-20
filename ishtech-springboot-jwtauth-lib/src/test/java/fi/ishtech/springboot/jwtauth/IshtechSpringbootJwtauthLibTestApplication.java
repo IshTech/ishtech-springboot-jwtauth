4e2975b235d6f31d2969ea2d703b60319224cc8b
Muneer Ahmed Syed <muneer@ishtech.fi>
2026-04-20 03:21:00 +0300

    junit for ishtech-springboot-jwtauth-lib

---
+++ ishtech-springboot-jwtauth-lib/src/test/java/fi/ishtech/springboot/jwtauth/IshtechSpringbootJwtauthLibTestApplication.java
@@ -0,0 +1,13 @@
+package fi.ishtech.springboot.jwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshtechSpringbootJwtauthLibTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshtechSpringbootJwtauthLibTestApplication.class, args);
+	}
+
+}
